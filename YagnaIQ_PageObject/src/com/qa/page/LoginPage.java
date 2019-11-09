bb1b54d16688718560511adc0d91a5183757842a
tushar <tushar@Tushar-Patil>
2019-11-10 00:33:35 +0530

    new code

--- YagnaIQ_PageObject/src/com/qa/page/LoginPage.java
+++ YagnaIQ_PageObject/src/com/qa/page/LoginPage.java
@@ -1,5 +1,20 @@
 package com.qa.page;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import com.qa.base.TestBase;
+
+public class LoginPage extends TestBase{
+		
+		//Page Factory - OR:
+		@FindBy(name="userName")
+		WebElement username;
+		
+		@FindBy(name="password")
+		WebElement password;
+
+		
+		@FindBy(id="loginButtonFrontView")
+		WebElement loginBtn; 
 }
---
+++ YagnaIQ_PageObject/src/com/qa/test/LoginTest.java
@@ -0,0 +1,5 @@
+package com.qa.test;
+
+public class LoginTest {
+
+}
