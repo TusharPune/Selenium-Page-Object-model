3c62df48490956791fa691803add76086d929f25
tushar <tushar@Tushar-Patil>
2019-11-14 23:17:07 +0530

    Final Report

--- YagnaIQ_PageObject/src/com/qa/ExtentReportListener/ExtentReporterNG2.java
+++ YagnaIQ_PageObject/src/com/qa/ExtentReportListener/ExtentReporterNG2.java
@@ -21,7 +21,9 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class ExtentReporterNG implements IReporter {
+
+
+public class ExtentReporterNG2 implements IReporter {
 	private ExtentReports extent;
 
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
