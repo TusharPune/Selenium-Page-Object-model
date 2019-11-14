09c67748c21cabf6a47f79365af2c534128d2f9c
tushar <tushar@Tushar-Patil>
2019-11-14 22:57:03 +0530

    zcvx

--- YagnaIQ_PageObject/src/com/qa/ExtentReportListener/ExtentReporterNG.java
+++ YagnaIQ_PageObject/src/com/qa/ExtentReportListener/ExtentReporterNG.java
@@ -27,7 +27,7 @@ public class ExtentReporterNG implements IReporter {
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites,
 			String outputDirectory) {
 		extent = new ExtentReports(outputDirectory + File.separator
-				+ "Extent.html", true);
+				+ "tushar.html", true);
 
 		for (ISuite suite : suites) {
 			Map<String, ISuiteResult> result = suite.getResults();
